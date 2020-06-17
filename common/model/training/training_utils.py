4668ab6d11636705a2268b84b5d707def31c0af9
mdunlop <matthewdunlop22@gmail.com>
2020-06-17 15:35:45 +0100

    Explore n3060_std dataset

---
+++ common/model/training/training_utils.py
@@ -0,0 +1,77 @@
+'''
+Stores handy model evaluation scripts
+'''
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, precision_recall_curve, auc, make_scorer, recall_score, accuracy_score, precision_score, confusion_matrix
+import pandas as pd
+import numpy as np
+
+def adjusted_classes(y_scores, t):
+    """
+    This function adjusts class predictions based on the prediction threshold (t).
+    Will only work for binary classification problems.
+    """
+    return [1 if y >= t else 0 for y in y_scores]
+
+def precision_recall_threshold(p, r, thresholds, y_scores, y_test, t=0.5):
+    """
+    plots the precision recall curve and shows the current value for each
+    by identifying the classifier's threshold (t).
+    """
+    
+    # generate new class predictions based on the adjusted_classes
+    # function above and view the resulting confusion matrix.
+    y_pred_adj = adjusted_classes(y_scores, t)
+    cm = pd.DataFrame(confusion_matrix(y_test, y_pred_adj),
+                       columns=['pred_neg', 'pred_pos'], 
+                       index=['neg', 'pos'])
+    print(cm)
+    
+    print("Sensitivity (Recall): {}".format(cm.loc["pos","pred_pos"]/(cm.loc["pos"].sum())))
+    print("Specificity         : {}".format(cm.loc["neg","pred_neg"]/(cm.loc["neg"].sum())))
+    
+    # plot the curve
+    plt.figure(figsize=(8,8))
+    plt.title("Precision and Recall curve")
+    plt.step(r, p, color='b', alpha=0.2,
+             where='post')
+    plt.fill_between(r, p, step='post', alpha=0.2,
+                     color='b')
+    plt.ylim([0.0, 1.01]);
+    plt.xlim([0.0, 1.01]);
+    plt.xlabel('Recall');
+    plt.ylabel('Precision');
+    
+    # plot the current threshold on the line
+    close_default_clf = np.argmin(np.abs(thresholds - t))
+    plt.plot(r[close_default_clf], p[close_default_clf], '^', c='k',
+            markersize=15)
+
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    """
+    Modified from:
+    Hands-On Machine learning with Scikit-Learn
+    and TensorFlow; p.89
+    """
+    plt.figure(figsize=(8, 8))
+    plt.title("Precision and Recall Scores as a function of the decision threshold")
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
+    plt.ylabel("Score")
+    plt.xlabel("Decision Threshold")
+    plt.legend(loc='best')
+
+def plot_roc_curve(fpr, tpr, label=None):
+    """
+    The ROC curve, modified from 
+    Hands-On Machine learning with Scikit-Learn and TensorFlow; p.91
+    """
+    plt.figure(figsize=(8,8))
+    plt.title('ROC Curve')
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.axis([-0.005, 1, 0, 1.005])
+    plt.xticks(np.arange(0,1, 0.05), rotation=90)
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate (Recall)")
+    plt.legend(loc='best')
